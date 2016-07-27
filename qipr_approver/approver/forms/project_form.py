cbf3a7d5a9a84b4b82f2a66e2e3c0c1b4eb3230a
ndavuluri <samanthadavuluri4444@gmail.com>
2016-07-27 16:55:02 -0400

    Adding Keywords to Project Form

--- qipr_approver/approver/forms/project_form.py
+++ qipr_approver/approver/forms/project_form.py
@@ -1,5 +1,7 @@
 from django.utils import timezone
 from approver.models import Project
+from approver.models import Keyword
+from approver.models import User
 from approver import utils
 
 class ProjectForm():
@@ -11,6 +13,11 @@ class ProjectForm():
                       'label': 'Title',
                       'type': 'text',
                       'value': project.title or ''}
+        
+        self.keywords = {'name': 'keywords',
+                      'label': 'Keywords',
+                      'options': [item.name for item in Keyword.objects.all()],
+                      'selected': self.keywords_or_empty(project)}
 
         self.description = {'name': 'description',
                             'label': 'Description',
@@ -28,4 +35,5 @@ class ProjectForm():
                                    'label': 'Proposed End Date',
                                    'type': 'date',
                                    'value': utils.format_date(end_date)}
-
+    def keywords_or_empty(self,project):
+      return [item.name for item in project.keywords.all()] if project.title else []
