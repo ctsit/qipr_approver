828696bcc574b5623871318a2972d10959060c88
Patrick White <pfwhite@ufl.edu>
2016-07-28 09:48:36 -0400

    changes to form_input template tag

--- qipr_approver/approver/templatetags/form_input.py
+++ qipr_approver/approver/templatetags/form_input.py
@@ -10,7 +10,6 @@ def form_input(input_dict):
 
     name: the name that corresponds to the input, will be in form when submitted
     label: the string that will be displayed as a label
-    html_id: the id that can be used as an html_selector for the input
     type: the input type. See w3 schools input types for information
     value: the value of the input. Not checked or validated
     """
