7d3096f8f88f00aae957810ae7088b9d01014fc5
Matthew McConnell <bigmatt0211@ufl.edu>
2016-08-03 16:18:39 -0400

    adds a counter parameter to the address view and updates the html to use it.

--- qipr_approver/approver/templatetags/address.py
+++ qipr_approver/approver/templatetags/address.py
@@ -4,16 +4,18 @@ register = template.Library()
  
 
 @register.inclusion_tag('templatetags/address.html')
-def address(address_model, address_type):
+def address(address_model, address_type, counter=''):
     """
     Renders the address widget
     Takes the following keywords:
 
     address_model: the address object passed from the user
     address_type: the type of address used to id the div
+    optional counter: adds a counter to the html names to help make them unique
     """
     input_dict = {
         'address_type': address_type,
+        'counter': counter,
         'address1': address_model.address1,
         'address2': address_model.address2,
         'city': address_model.city,
