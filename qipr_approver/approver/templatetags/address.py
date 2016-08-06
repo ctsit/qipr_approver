151b216de9bf5df921d57b1db6fcc334b4927b1d
Matthew McConnell <bigmatt0211@ufl.edu>
2016-08-06 11:14:21 -0400

    Update address template to include address id if available

--- qipr_approver/approver/templatetags/address.py
+++ qipr_approver/approver/templatetags/address.py
@@ -7,20 +7,21 @@ register = template.Library()
 def address(address_model, address_type, counter=''):
     """
     Renders the address widget
-    Takes the following keywords:
+    Takes the following parameters:
 
-    address_model: the address object passed from the user
-    address_type: the type of address used to id the div
-    optional counter: adds a counter to the html names to help make them unique
+    * address_model: the address object passed from the user
+    * address_type: the type of address used to id the div
+    * optional counter: adds a counter to the html names to help make them unique
     """
     input_dict = {
         'address_type': address_type or '',
+        'address_id': address_model.id or '',
         'counter': counter,
         'address1': address_model.address1 or '',
         'address2': address_model.address2 or '',
         'city': address_model.city or '',
         'zip_code': address_model.zip_code or '',
         'state': address_model.state or '',
-        'country': address_model.country or ''
+        'country': address_model.country or '',
     }
     return input_dict
