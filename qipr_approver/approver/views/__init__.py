a2070e683d17efe12e2da24bced142e9979f7879
ndavuluri <samanthadavuluri4444@gmail.com>
2016-08-08 17:50:33 -0400

    Delete Projects in Dashboard

--- qipr_approver/approver/views/__init__.py
+++ qipr_approver/approver/views/__init__.py
@@ -1,6 +1,6 @@
 from approver.views.aboutyou import about_you
 from approver.views.index import index
-from approver.views.dashboard import dashboard,project_del
+from approver.views.dashboard import dashboard
 from approver.views.fake_shib import fake_shib
 from approver.views.first_login import first_login
 from approver.views.index import index
--- qipr_approver/approver/views/dashboard.py
+++ qipr_approver/approver/views/dashboard.py
@@ -17,16 +17,34 @@ from django.shortcuts import render
 from django.utils import timezone
 
 @login_required
-def dashboard(request):
-    project_title = []
-    projects = []
-    context = {
-        'content': 'approver/dashboard.html',
-        'projects' : get_project_context(request),
-        'toast_text' : utils.get_and_reset_toast(request.session)
-    }
+def dashboard(request,project_id=None):
+    if request.method == 'GET':
+        project_title = []
+        projects = []
+        context = {
+            'content': 'approver/dashboard.html',
+            'projects': get_project_context(request),
+            'toast_text': utils.get_and_reset_toast(request.session)
+        }
+        return utils.layout_render(request, context)
+    elif request.method == 'POST':
+        current_user = User.objects.get(username=utils.get_current_user_gatorlink(request.session))
 
-    return utils.layout_render(request, context)
+        if(project_id is not None):
+            project = project_crud.get_project_or_none(project_id)
+            if(project is None):
+                return utils.dashboard_redirect_and_toast(request, 'Project with id {} does not exist.'.format(project_id))
+            else:
+                if(project_crud.curent_user_is_project_owner(current_user, project) is not True):
+                        return utils.dashboard_redirect_and_toast(request, 'You are not authorized to delete this project.')
+                else:
+                    if (project.get_is_editable() is not True):
+                        return utils.dashboard_redirect_and_toast(request, 'You are not allowed to delete/edit this project.')
+                    else:
+                        project.delete(current_user)
+                        return redirect(reverse("approver:dashboard"))
+        else:
+            return redirect(reverse("approver:dashboard"))
 
 def get_project_context(request):
     username = request.session.get(constants.SESSION_VARS['gatorlink'])
@@ -39,30 +57,3 @@ def get_project_context(request):
 def __get_project_tuples(project, role):
     return (project.title,
             project.pk,role)
-
-@login_required
-def project_del(request, project_id=None):
-    context = {
-        'content': 'approver/dashboard.html',
-        'project_id': project_id,
-    }
-    current_user = User.objects.get(username=utils.get_current_user_gatorlink(request.session))
-
-    if request.method == 'GET':
-        if(project_id is not None):
-            project = project_crud.get_project_or_none(project_id)
-            if(project is None):
-                return utils.dashboard_redirect_and_toast(request, 'Project with id {} does not exist.'.format(project_id))
-            else:
-                if(project_crud.curent_user_is_project_owner(current_user, project) is not True):
-                        return utils.dashboard_redirect_and_toast(request, 'You are not authorized to delete this project.')
-                else:
-                    if (project.get_is_editable() is not True):
-                        return utils.dashboard_redirect_and_toast(request, 'You are not allowed to delete/edit this project.')
-                    else:
-                        Project.objects.filter(id=project_id).delete()
-                        return redirect(reverse("approver:dashboard"))
-        else:
-            return utils.layout_render(request, context)
-    else:
-        return utils.layout_render(request, context)
