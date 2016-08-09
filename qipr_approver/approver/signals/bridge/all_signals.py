70ecd7f6d332ccf42407561f152bde8cce8f88f6
Patrick White <pfwhite@ufl.edu>
2016-08-09 10:39:59 -0400

    Import bug fix and doc update

--- qipr_approver/approver/signals/bridge/all_signals.py
+++ qipr_approver/approver/signals/bridge/all_signals.py
@@ -1,4 +1,4 @@
-from approver.signals.bridge.project_signals import connect_model_signals
+from approver.signals.bridge.model_signals import connect_model_signals
 
 def connect_signals():
     connect_model_signals()
