1d33b4b3350f92fa1e67343d75b1d5166ff27bf3
Patrick White <pfwhite@ufl.edu>
2016-08-12 12:56:23 -0400

    more testing. Better directory structure

---
+++ qipr_approver/approver/tests/tests_models/test_person.py
@@ -0,0 +1,35 @@
+import sys
+
+from approver.models import *
+from approver.signals.bridge.model_signals import model_push
+
+import django
+from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
+from django.db.models import fields, signals
+from django.test import TestCase, Client
+
+class PersonTestCase(TestCase):
+    def setUp(self):
+        signals.post_save.disconnect(model_push, Person)
+        self.user = User(username='testuser')
+        self.user.save()
+        self.person = Person()
+
+    def test_person_needs_user_to_save(self):
+        self.assertRaises(TypeError, lambda : self.person.save())
+        try:
+            self.person.save(self.user)
+        except:
+            self.fail('The person failed to save with user. Exception:' + str(sys.exc_info()))
+
+    def test_person_model_has_correct_fields(self):
+        person = Person()
+        for field in Person._meta.fields:
+            if field.name in ["first_name", "last_name"]:
+                self.assertEqual(isinstance(field, django.db.models.fields.CharField), True)
+                self.assertEqual(field.max_length,30)
+            if field.name in ["webpage_url", "email_address"]:
+                self.assertEqual(isinstance(field, django.db.models.fields.CharField),True)
+            if field.name in ["business_phone", "contact_phone"]:
+                self.assertEqual(isinstance(field, django.db.models.fields.CharField),True)
