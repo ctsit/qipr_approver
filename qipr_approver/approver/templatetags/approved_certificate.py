56b448bce5d78a27dbf544ef217f806d7d6be8cd
Matthew McConnell <bigmatt0211@ufl.edu>
2016-08-15 09:28:35 -0400

    Update certificate context to begin page layout

--- qipr_approver/approver/templatetags/approved_certificate.py
+++ qipr_approver/approver/templatetags/approved_certificate.py
@@ -1,7 +1,15 @@
 from django import template
 
+from approver.models import Project
+
 register = template.Library()
 
 @register.inclusion_tag('templatetags/approved_certificate.html')
-def approved_certificate():
-    return {}
+def approved_certificate(project):
+    project_details = {
+        'project_owner_first_name': project.owner.first_name,
+        'project_owner_last_name': project.owner.last_name,
+        'project_title': project.title,
+        'project_collab': project.collaborator.all(),
+    }
+    return project_details
