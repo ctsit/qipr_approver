7e72e2559262e0b5e8c61a20b21527ea4b1493c3
Matthew McConnell <bigmatt0211@ufl.edu>
2016-08-31 16:43:42 -0400

    update function description

--- qipr_approver/approver/templatetags/form_input_textarea.py
+++ qipr_approver/approver/templatetags/form_input_textarea.py
@@ -5,12 +5,11 @@ register = template.Library()
 @register.inclusion_tag('templatetags/form_input_textarea.html')
 def form_input_textarea(input_dict, is_disabled=False):
     """
-    Renders an input with a label.
+    Renders a textarea with a label.
     Takes the following keywords:
 
     name: the name that corresponds to the input, will be in form when submitted
     label: the string that will be displayed as a label
-    type: the input type. See w3 schools input types for information
     value: the value of the input. Not checked or validated
     """
     input_classes = input_dict.get('input_classes') or []
