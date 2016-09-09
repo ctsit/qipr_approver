3e41021b0a8f32eed9fe5720dead7d1074915db2
Matthew McConnell <bigmatt0211@ufl.edu>
2016-09-09 09:55:57 -0400

    check if user is logged in before showing landing page

--- qipr_approver/approver/views/index.py
+++ qipr_approver/approver/views/index.py
@@ -5,6 +5,12 @@ from approver.forms import AboutYouForm, ProjectForm
 from django.contrib.auth.models import User
 from approver.models import Project
 
+import approver.constants as constants
+import approver.utils as utils
+from approver.views.dashboard import dashboard
 
 def index(request):
-    return render(request, 'approver/index.html')
+    if(utils.get_current_user_gatorlink(request.session)):
+        return dashboard(request)
+    else:
+        return render(request, 'approver/index.html')
