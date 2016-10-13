3b145d677e86ef9c98bc9c747e6e027533b945ac
Matthew McConnell <bigmatt0211@ufl.edu>
2016-10-13 16:30:15 -0400

    Adds the required Nursing Units to the Clinical Area dropdown and updates the label to include the word units

--- qipr_approver/approver/forms/project_form.py
+++ qipr_approver/approver/forms/project_form.py
@@ -38,7 +38,7 @@ class ProjectForm():
                         'selected': utils.get_related_property(project, "big_aim")}
 
         self.clinical_area = {'name': 'clinical_area',
-                               'label': 'Clinical Area',
+                               'label': 'Clinical Area/Unit',
                                'options': filter(utils.is_not_none, [item.name for item in ClinicalArea.objects.all()]),
                                'selected': utils.get_related_property(project,"clinical_area")}
 
