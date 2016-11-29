a87f024e42647bdaf3edb0aede0c5e80cb4c5373
Matthew McConnell <bigmatt0211@ufl.edu>
2016-11-29 10:54:19 -0500

    Use SHIB_ENABLED to create or validate a session

--- qipr_approver/approver/decorators/login_required.py
+++ qipr_approver/approver/decorators/login_required.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect
 from django.core.urlresolvers import reverse
 
-from approver.constants import SESSION_VARS
+from approver.constants import SESSION_VARS, SHIB_ENABLED
 
 def login_required(view_function):
     """
@@ -10,7 +10,11 @@ def login_required(view_function):
     """
     def wrapped_view(request, *args, **kwargs):
         if request.session.get(SESSION_VARS.get('gatorlink')):
-            return view_function(request, *args, **kwargs)
-        else:
-            return redirect(reverse("approver:shib"))
+            if __shib_validated(request):
+                return view_function(request, *args, **kwargs)
+        return redirect(reverse("approver:shib"))
+
+    def __shib_validated(request):
+        return ((SHIB_ENABLED == 'true' and request.META.get('HTTP_EPPN')) or (SHIB_ENABLED == 'false'))
+
     return wrapped_view
--- qipr_approver/approver/views/fake_shib.py
+++ qipr_approver/approver/views/fake_shib.py
@@ -3,13 +3,21 @@ from django.http import HttpResponse, Http404
 from django.views.decorators.csrf import csrf_protect
 
 from approver.workflows import shib
+from approver.constants import SHIB_ENABLED
 
 #TODO: implement real shib and get rid of this fake shib endpoint
 @csrf_protect
 def fake_shib(request):
-    if request.method == "GET":
-        return render(request, 'approver/fakeshib.html')
-    elif request.method == "POST":
-        return shib.after_validation(request)
+    if SHIB_ENABLED == 'false':
+        if request.method == "GET":
+            return render(request, 'approver/fakeshib.html')
+        elif request.method == "POST":
+            return shib.after_validation(request)
+        else:
+            raise Http404('This url only supports POST and GET.')
+
     else:
-        raise Http404('This url only supports POST and GET.')
+        if (request.META.get('HTTP_EPPN') and SHIB_ENABLED == 'true'):
+            return shib.after_validation(request)
+        else:
+            raise Http404('There is a problem with your Shibboleth django settings')
