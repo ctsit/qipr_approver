ae07e6c4c3df6125e91a4c8ef9bfaf26860c38b2
Amber Allen <amberlallen@ufl.edu>
2016-12-19 11:02:18 -0500

    Add placeholder and label text to project form

--- qipr_approver/approver/forms/project_form.py
+++ qipr_approver/approver/forms/project_form.py
@@ -19,14 +19,16 @@ class ProjectForm():
 
         self.collaborator = {'name': 'collaborator',
                              'model': 'person',
-                             'placeholder': 'Type collaborator name, then click "enter" to save',
+                             'placeholder': 'e.g. Alligator, Albert',
+                             'label': 'Type collaborator name, then press "enter" to save',
                              'filter_field': 'email_address',
                              'options': filter(utils.is_not_none, [item.email_address for item in Person.objects.all()]),
                              'selected': utils.get_related_property(project, "collaborator", 'email_address')}
 
         self.advisor = {'name': 'advisor',
                         'model': 'person',
-                        'placeholder': 'Type advisor name, then click "enter" to save',
+                        'placeholder': 'e.g. Alligator, Alberta',
+                        'label': 'Type advisor name, then press "enter" to save',
                         'filter_field': 'email_address',
                         'options': filter(utils.is_not_none, [item.email_address for item in Person.objects.all()]),
                         'selected': utils.get_related_property(project, "advisor", 'email_address')}
@@ -34,36 +36,37 @@ class ProjectForm():
         self.keyword = {'name': 'keyword',
                         'label': 'Keywords',
                         'model': 'keyword',
-                        'placeholder': 'Type keyword, then click "enter" to save',
+                        'placeholder': 'e.g. Micronutrient and/or Zinc',
+                        'label': 'Please indicate 5 or more keywords relating to your project. Type keyword, then press "enter" to save',
                         'filter_field': 'name',
                         'options': filter(utils.is_not_none, [item.name for item in Keyword.objects.all()]),
                         'selected': utils.get_related_property(project, "keyword"),
                         'div_classes': 'about__txtfield--100'}
 
         self.big_aim = {'name': 'big_aim',
-                        'label': 'UF Health Big Aims',
+                        'label': 'Please indicate the UF Health Big Aims relating to your project. Type keyword, then press "enter" to save',
                         'model': 'bigaim',
-                        'placeholder': 'Type big aim, then click "enter" to save',
+                        'placeholder': 'e.g. Zero Harm and/or Increase Value',
                         'filter_field': 'name',
                         'options': filter(utils.is_not_none, [item.name for item in BigAim.objects.all()]),
                         'selected': utils.get_related_property(project, "big_aim"),
                         'div_classes': 'about__txtfield--100'}
 
         self.clinical_area = {'name': 'clinical_area',
-                              'label': 'Clinical Area/Unit',
+                              'label': 'OPTIONAL: What is the Clinical Area/Unit of your project? Type clinical area, then press "enter" to save',
                               'options': filter(utils.is_not_none, [item.name for item in ClinicalArea.objects.all()]),
                               'selected': utils.get_related_property(project,"clinical_area"),
                               'model': 'clinicalarea',
-                              'placeholder': 'Type clinical area, then click "enter" to save',
+                              'placeholder': 'e.g. NICU 3 and/or Unit 64',
                               'filter_field': 'name',
                               'options': filter(utils.is_not_none, [item.name for item in ClinicalArea.objects.all()]),
                               'selected': utils.get_related_property(project,"clinical_area"),
                               'div_classes': 'about__txtfield--100'}
 
         self.clinical_setting = {'name': 'clinical_setting',
-                                 'label': 'Clinical Setting',
+                                 'label': 'What is the Clinical Setting of your project? Type clinical setting, then press "enter" to save',
                                  'model': 'clinicalsetting',
-                                 'placeholder': 'Type clinical setting, then click "enter" to save',
+                                 'placeholder': 'e.g. NICU and/or General Medicine.',
                                  'filter_field': 'name',
                                  'options': filter(utils.is_not_none, [item.name for item in ClinicalSetting.objects.all()]),
                                  'selected': utils.get_related_property(project,"clinical_setting"),
@@ -73,7 +76,7 @@ class ProjectForm():
                             'type': 'text',
                             
                             'input_classes': ['description__height'], 
-                            'placeholder': 'Give a brief description about your Quality Improvement project (up to 250 words)',
+                            'placeholder': 'Give a description of your Quality Improvement project here (Please use at least 250 words. When filled, this input box holds roughly 250 words.)',
                             'value': project.description or ''}
 
         self.proposed_start_date = {'name': 'proposed_start_date',
