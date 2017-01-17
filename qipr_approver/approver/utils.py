4a7dd17d9e47a0bde7d26d0a98d0c7041a0c6171
Matthew McConnell <bigmatt0211@ufl.edu>
2017-01-17 13:47:59 -0500

    Add the FAQ url to the nabber

--- qipr_approver/approver/utils.py
+++ qipr_approver/approver/utils.py
@@ -43,6 +43,7 @@ def layout_render(request, context):
     """
     context['base_url'] = constants.base_url
     context['registry_search_url'] = constants.registry_search_path
+    context['faq_url'] = constants.registry_hostportpath
     return render(request, 'approver/layout.html', context)
 
 def get_current_user_gatorlink(request):
