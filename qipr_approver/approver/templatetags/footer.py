2acab30d26d96becb35a9892a08c8096f35815d3
Matthew McConnell <bigmatt0211@ufl.edu>
2017-04-26 17:01:13 -0400

    Fix bug with footer having the wrong pending project count

--- qipr_approver/approver/templatetags/footer.py
+++ qipr_approver/approver/templatetags/footer.py
@@ -18,7 +18,7 @@ def footer():
         'count_active_users': 0,
         'count_approved_projects': 0,
     }
-    input_dict['count_registered_projects'] = Project.objects.count()
+    input_dict['count_registered_projects'] = Project.objects.exclude(approval_date__isnull=False).count()
     input_dict['count_monthly_projects'] = Project.objects.filter(created__month=timezone.now().month).count()
     input_dict['count_active_users'] = User.objects.exclude(last_login=None).count()
     input_dict['count_approved_projects'] = Project.objects.exclude(approval_date=None).count()
